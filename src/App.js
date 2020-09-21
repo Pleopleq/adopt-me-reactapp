82b876ad7c49036a33181c5bc54133c98286826d
Felix Anducho <felix.anducho74@gmail.com>
2020-09-21 17:21:59 -0500

    Add a modal using portals. Wrap Details component into ErrorBoundary to prevent future errors with the API

--- src/App.js
+++ src/App.js
@@ -1,20 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import { render } from "react-dom";
 import { Router, Link } from "@reach/router";
 import Details from "./Details";
 import SearchParams from "./SearchParams";
+import ThemeContext from "./ThemeContext";
 
 const App = () => {
+  const themeHook = useState("darkblue");
   return (
-    <div>
-      <Link to="/">
-        <h1>Adopt me</h1>
-      </Link>
-      <Router>
-        <SearchParams path="/"></SearchParams>
-        <Details path="/details/:id"></Details>
-      </Router>
-    </div>
+    <ThemeContext.Provider value={themeHook}>
+      <div>
+        <Link to="/">
+          <h1>Adopt me</h1>
+        </Link>
+        <Router>
+          <SearchParams path="/"></SearchParams>
+          <Details path="/details/:id"></Details>
+        </Router>
+      </div>
+    </ThemeContext.Provider>
   );
 };
 
--- src/Details.js
+++ src/Details.js
@@ -1,6 +1,9 @@
 import React from "react";
 import Carousel from "./Carousel";
 import pet from "@frontendmasters/pet";
+import ErrorBoundary from "./ErrorBoundary";
+import { navigate } from "@reach/router";
+import Modal from "./Modal";
 
 class Details extends React.Component {
   constructor(props) {
@@ -8,12 +11,14 @@ class Details extends React.Component {
 
     this.state = {
       loading: true,
+      showModal: false,
     };
   }
 
   componentDidMount() {
     pet.animal(this.props.id).then(({ animal }) => {
       this.setState({
+        url: animal.url,
         name: animal.name,
         animal: animal.type,
         location: `${animal.contact.address.city}, ${animal.contact.address.state}`,
@@ -25,21 +30,48 @@ class Details extends React.Component {
     }, console.error);
   }
 
+  toggleModal = () => this.setState({ showModal: !this.state.showModal });
+  adopt = () => navigate(this.state.url);
+
   render() {
     if (this.state.loading) {
       return <h1>loading....</h1>;
     }
-    const { animal, breed, location, description, name, media } = this.state;
+    const {
+      animal,
+      breed,
+      location,
+      description,
+      name,
+      media,
+      showModal,
+    } = this.state;
+
     return (
       <div>
         <Carousel media={media}></Carousel>
         <h1>{name}</h1>
         <h2>{`${animal} - ${breed} - ${location}`}</h2>
         <p>{description}</p>
-        <button>Adopt {name}</button>
+        {showModal ? (
+          <Modal>
+            <div>
+              <h1>Would you like to adopt {name}?</h1>
+              <button onClick={this.adopt}>Yes</button>
+              <button onClick={this.toggleModal}>No.</button>
+            </div>
+          </Modal>
+        ) : null}
+        <button onClick={this.toggleModal}>Adopt {name}</button>
       </div>
     );
   }
 }
 
-export default Details;
+export default function DetailsWithErrorBoundary(props) {
+  return (
+    <ErrorBoundary>
+      <Details {...props}></Details>
+    </ErrorBoundary>
+  );
+}
